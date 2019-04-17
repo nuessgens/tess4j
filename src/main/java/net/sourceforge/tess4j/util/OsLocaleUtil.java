345050518645018fff84f0e86eb29ec3246bc753
Christian Nüssgens <christian@nuessgens.com>
2019-04-17 19:52:36 +0200

    Set locale for whole java process

---
+++ src/main/java/net/sourceforge/tess4j/util/OsLocaleUtil.java
@@ -0,0 +1,38 @@
+package net.sourceforge.tess4j.util;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
+
+public final class OsLocaleUtil {
+    private static final String LOCALE = "C";
+    private static String restoreLocale;
+
+    public static void initLocale() {
+        restoreLocale = CLibrary.INSTANCE.setlocale(CLibrary.LC_CTYPE, null);
+        CLibrary.INSTANCE.setlocale(CLibrary.LC_CTYPE, LOCALE);
+    }
+
+    public static void restoreLocale() {
+        if (restoreLocale != null) {
+            CLibrary.INSTANCE.setlocale(CLibrary.LC_CTYPE, restoreLocale);
+            restoreLocale = null;
+        }
+    }
+
+    private static interface CLibrary extends Library {
+        CLibrary INSTANCE = Native.load((Platform.isWindows() ? "msvcrt" : "c"), CLibrary.class);
+
+
+        int LC_CTYPE = 0;
+        int LC_NUMERIC = 1;
+        int LC_ALL = 6;
+
+        // char *setlocale(int category, const char *locale);
+        String setlocale(int category, String locale);
+    }
+
+    private OsLocaleUtil() {
+
+    }
+}
